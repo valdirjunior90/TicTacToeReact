f3edc2374626d416f301baf236f45a8dc20acf5a
Valdir Junior <valdir@solarz.com.br>
2021-08-21 13:17:02 -0300

    Feat. completo

---
+++ src/TicTacToe.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import './TicTacToe.css';
+
+const TicTacToe = () => {
+  const [turn, setTurn] = useState('x');
+  const [cells, setCells] = useState(Array(9).fill(''));
+  const [winner, setWinner] = useState();
+
+  const checkForWinner = (squares)=>{
+    let combos = {
+      across:[
+          [0,1,2],
+          [3,4,5],
+          [6,7,8],
+      ],
+      down:[
+          [0,3,6],
+          [1,4,7],
+          [2,5,8],
+      ],
+      diagonal:[
+          [0,4,8],
+          [2,4,6],
+      ]
+    }
+
+    for(let combo in combos){
+        combos[combo].forEach(pattern => {
+          if(
+            squares[pattern[0]] === '' ||
+            squares[pattern[1]] === '' ||
+            squares[pattern[2]] === ''
+          ) {
+
+          }else if(
+            squares[pattern[0]] === squares[pattern[1]] && 
+            squares[pattern[1]] === squares[pattern[2]]
+          ){
+            setWinner(squares[pattern[0]]);
+          }
+        });
+    }
+  };
+
+
+
+  const handleClick = (num)=>{
+    if(cells[num] !== ''){
+      alert("ja foi clicado")
+      return;
+    }
+
+    let squares = [...cells];
+
+    if(turn === 'X'){
+      squares[num] = 'X';
+      setTurn('O')
+    }else{
+      squares[num] = 'O';
+      setTurn('X')
+    }
+
+    checkForWinner(squares)
+
+    setCells(squares)
+    console.log(squares)
+  }
+
+  const handleRestart = ()=>{
+    setWinner(null);
+    setCells(Array(9).fill(''))
+  }
+
+  const Cell = ({num})=>{
+    return <td className="cell" onClick={()=>handleClick(num)}>{cells[num]}</td>
+  }
+
+  return (
+    <div className="container">
+      <p>Turno: {turn}</p>
+      <table>
+        <tbody>
+          <tr>
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
+          </tr>
+          <tr>
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
+          </tr>
+          <tr>
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
+          </tr>
+        </tbody>
+      </table>
+      {winner && (
+        <>
+        <p><strong>{winner}</strong> é o ganhador</p>
+        
+        </>
+      )}
+      <button className="restart" onClick={()=>handleRestart()}>Recomeçar</button>
+    </div>
+  );
+}
+
+export default TicTacToe;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import TicTacToe from './TicTacToe';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <TicTacToe /> ,document.getElementById('root')
 );
 
